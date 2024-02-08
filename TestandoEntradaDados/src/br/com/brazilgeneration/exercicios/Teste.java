6a2312220107084252871d389e295fe8ed961b88
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-02-07 22:02:17 -0300

    novo commit para teste

---
+++ TestandoEntradaDados/src/br/com/brazilgeneration/exercicios/Teste.java
@@ -0,0 +1,22 @@
+package br.com.brazilgeneration.exercicios;
+
+import java.util.Scanner;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Digite a sua idade: ");
+		
+		int idade = sc.nextInt();
+		
+		
+		System.out.println(idade);
+		
+		sc.close();
+	}
+
+}
