815513eb5b432807d7f8e9e1523f61fdd1e7ac6e
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-02-08 17:08:53 -0300

    meuprojeto

---
+++ MeuPrimeiroProjeto/src/br/com/teste/Program.java
@@ -0,0 +1,12 @@
+package br.com.teste;
+
+public class Program {
+	
+	//Classe principal do Java
+	
+	public static void main (String[] args) {
+		System.out.println("seutextoaqui");
+		
+		System.out.println(":)");
+	}
+}
