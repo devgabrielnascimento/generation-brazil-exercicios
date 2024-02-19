550f6d5607dee3b7d863f9373503d714cbba08a9
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-02-19 11:34:29 -0300

    novo...

---
+++ helloWorld/src/turma72/MyFirstApplication.java
@@ -0,0 +1,15 @@
+package turma72;
+
+public class MyFirstApplication {
+	
+	public static void main(String[] args) {
+		
+		String nome;
+		nome = "seu nome";
+		
+		System.out.println(nome);
+		
+		System.out.println("Oi" + nome);
+		
+	}
+}
